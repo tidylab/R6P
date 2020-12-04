1a2cb1ae679469349de23d443bcf63bfb268c51f
harell <harel@loostiger.com>
2020-12-04 17:33:57 +1300

    generalize class name

--- R/creational-Singleton.R
+++ R/creational-Singleton.R
@@ -1,30 +1,17 @@
 #' @title Singleton Pattern
 #' @description Ensure a class only has one instance, and provide a global point
 #'   of access to it.
-#' @examples
-#' # Example: A Counter Implementation
-#' Counter <- R6::R6Class(inherit = Singleton, public = list(
-#'     count = 0,
-#'     add_1 = function(){self$count = self$count + 1; invisible(self)}
-#' ))
-#'
-#' counter <- Counter$new()
-#' counter$count
-#' counter$add_1()$count
-#' counter$add_1()$count
-#'
-#' retrieved_conter <- Counter$new()
-#' retrieved_conter$count
-#'
-#' rm(retrieved_conter)
 #' @references \href{Wikipedia}{https://en.wikipedia.org/wiki/Singleton_pattern}
 #' @family creational design patterns
 #' @export
 Singleton <- R6::R6Class("Singleton", public = list(
     #' @description Create or retrieve an object
     initialize = function(){
+        classname  <- gsub("\\$new\\(.*\\)", "", deparse(sys.calls()[[sys.nframe()-1]]))
+
         if(is.null(private$instance)){
-            Singleton$set("private", "instance", self, overwrite = TRUE)
+            command <- paste0(classname, "$set('private', 'instance', self, overwrite = TRUE)")
+            eval(parse(text = command))
             private$instance <- self
         } else {
             self <- private$instance
--- tests/testthat/test-creational-Singleton.R
+++ tests/testthat/test-creational-Singleton.R
@@ -12,3 +12,20 @@ test_that("calling Singleton$new instantiates identical objects", {
     expect_is(singleton2 <- Singleton$new(), "Singleton")
     expect_equal(singleton1, singleton2)
 })
+
+#' @examples
+#' # Example: A Counter Implementation
+#' Counter <- R6::R6Class(inherit = Singleton, public = list(
+#'     count = 0,
+#'     add_1 = function(){self$count = self$count + 1; invisible(self)}
+#' ))
+#'
+#' counter <- Counter$new()
+#' counter$count
+#' counter$add_1()$count
+#' counter$add_1()$count
+#'
+#' retrieved_conter <- Counter$new()
+#' retrieved_conter$count
+#'
+#' rm(retrieved_conter)
