0671b17ca97b236db8416aa43fb3f546b24d4f19
harell <harel@loostiger.com>
2021-07-31 17:00:20 +1200

    misc updates

--- R/zzz.R
+++ R/zzz.R
@@ -2,16 +2,14 @@
     options(
         usethis.quiet = TRUE
     )
-	
-    try(
-        packageStartupMessage(
-            paste(
-                "\n\033[44m\033[37m",
-                "\nWelcome to R6P",
-                "\nMore information, vignettes, and guides are available on the R6P project website:",
-                "\nhttps://tidylab.github.io/R6P/",
-                "\n\033[39m\033[49m",
-                sep="")
-        )
+
+    if(interactive()) packageStartupMessage(
+        paste(
+            "\n\033[44m\033[37m",
+            "\nWelcome to R6P",
+            "\nMore information, vignettes, and guides are available on the R6P project website:",
+            "\nhttps://tidylab.github.io/R6P/",
+            "\n\033[39m\033[49m",
+            sep="")
     )
 }#nocov end
