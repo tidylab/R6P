3b92c31f18e492b5f615fb7500509fcb2fbc314a
harell <harell@users.noreply.github.com>
2022-10-01 20:45:35 +0300

    add packages

---
+++ R/utils.R
@@ -0,0 +1,10 @@
+#' @noRd
+imports_field <- function() {
+    collections::dict
+    dplyr::filter
+    purrr::walk
+    R6::R6Class
+    stringr::str_glue
+    tibble::tibble
+    tidyr::drop_na
+}
