81b4c89ddef4925d3ea6c51a2c3da81051d50963
harell <harell@users.noreply.github.com>
2024-12-22 20:14:02 +1300

    update package extraneous files

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,9 @@
+# R6P 0.4.0
+
+- Dropped dependency on `purrr` package
+- Uses "|>" and therefore requires R 4.1.0
+- Revamp function documentation to pass CRAN checks
+
 # R6P 0.2.1
 
 - Update system files
--- inst/snippets/submitting-package.R
+++ inst/snippets/submitting-package.R
@@ -1,11 +1,16 @@
 devtools::check(remote = TRUE, manual = TRUE)
 
-rhub::check( platform="windows-x86_64-devel", env_vars=c(R_COMPILE_AND_INSTALL_PACKAGES = "always") )
+rhub::check(platform="windows-x86_64-devel", env_vars=c(R_COMPILE_AND_INSTALL_PACKAGES = "always"))
+# rhub::rhub_setup()
+# rhub::rhub_check(platforms = c("windows", "macos", "linux"), branch = "master")
 
-devtools::check_win_devel()
+# devtools::check_win_devel()
+devtools::check_win_release()
 
 devtools::build_manual()
 
 devtools::spell_check()
 
 utils::maintainer(pkgload::pkg_name())
+
+# devtools::submit_cran()
